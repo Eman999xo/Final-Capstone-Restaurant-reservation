415d72bcb69ac13b79eca151ba19579d895c09aa
eman999xo <emanualtarway@gmail.com>
2022-11-09 17:30:53 -0500

    FinalChanges

--- back-end/src/db/seeds/01-tables.js
+++ back-end/src/db/seeds/01-tables.js
@@ -1,11 +1,7 @@
 const tables = require("./01-tables.json");
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
-exports.seed = function(knex) {
-  return knex.raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE")
-  .then (function () {
-    return knex("tables").insert(tables)
-  });
+
+exports.seed = function (knex) {
+  return knex
+    .raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE")
+    .then(() => knex("tables").insert(tables));
 };
